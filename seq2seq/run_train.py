1e11dc9eab9ca99d6eaf431ac7ff4a2cfd9c8796
Tiago de Lima <tiago.blima02@gmail.com>
2022-10-16 15:43:45 -0300

    adapting t5 entity recognizer

--- seq2seq/run_train.py
+++ seq2seq/run_train.py
@@ -228,11 +228,6 @@ class DataTrainingArguments:
     )
 
     def __post_init__(self):
-        if self.dataset_name is None and self.train_file is None and self.validation_file is None:
-            raise ValueError("Need either a dataset name or a training/validation file.")
-        elif self.source_lang is None or self.target_lang is None:
-            raise ValueError("Need to specify the source language and the target language.")
-
         # accepting both json and jsonl file extensions, as
         # many jsonlines files actually have a .json extension
         valid_extensions = ["json", "jsonl"]

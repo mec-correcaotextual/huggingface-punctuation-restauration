fe8ad2a66558c2c99e13cffbb9924e4072740c17
Tiago de Lima <tiago.blima02@gmail.com>
2022-10-19 16:41:26 -0300

    solving push to hub error

--- seq2seq/run_train.py
+++ seq2seq/run_train.py
@@ -49,7 +49,7 @@ from transformers import (
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
-
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.23.0.dev0")
 
